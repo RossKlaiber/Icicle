ee4a4ba8aff3504ac761a89f8fa55cb37d0690c0
Ross Klaiber <rossk31@icloud.com>
2023-06-26 11:43:24 -0400

    Initial Commit

---
+++ Icicle/IcicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcicleApp.swift
+//  Icicle
+//
+//  Created by Ross Klaiber on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct IcicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
